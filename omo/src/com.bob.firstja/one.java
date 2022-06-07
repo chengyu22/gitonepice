bbe8b4196768ed5c413fb2acf72c4074fe6b37dc
chengyu22 <209545769@qq.com>
2022-06-07 16:46:14 +0800

    one

--- omo/src/com.bob.firstja/one.java
+++ omo/src/com.bob.firstja/one.java
@@ -1,9 +1,11 @@
+package com.bob.firstja;
+
 /**
  * @author bob
- * @data 2022/6/7-16:36
+ * @data 2022/6/7-16:45
  */
 public class one {
     public static void main(String[] args) {
-        System.out.println("上传的文件");
+        System.out.println("测试文件");
     }
 }
