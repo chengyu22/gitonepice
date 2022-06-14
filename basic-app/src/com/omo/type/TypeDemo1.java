88e2938cfa34d1131a62433fb9194b91f58c6eb0
chengyu22 <209545769@qq.com>
2022-06-14 09:40:06 +0800

    one

---
+++ basic-app/src/com/omo/type/TypeDemo1.java
@@ -0,0 +1,20 @@
+package com.omo.type;
+
+/**
+ * @author bob
+ * @data 2022/6/14-9:28
+ */
+public class TypeDemo1 {
+    public static void main(String[] args) {
+//        自动类型转换
+        byte a=20;
+        int b=a; //自动类型转换 大范围可以直接赋给小范围
+
+        byte a1=12; // 00001100  自动转换 符号位为0位数不够前面补0
+        double db=a1; //00000000 00000000 00000000 00001100
+
+        char ch ='a';
+        int b1=ch; //转换存储二进制转换为10进制 97 01100001
+
+    }
+}
--- omo/src/com.bob.firstja/typesize.java
+++ omo/src/com.bob.firstja/typesize.java
@@ -5,4 +5,47 @@ package com.bob.firstja;
  * @data 2022/6/13-16:28
  */
 public class typesize {
+    public static void main(String[] args) {
+//        基本数据类型
+//       1 byte字节型占一个字节8位 -128 - 127
+        byte number=97;
+        System.out.println(number);
+//       byte number2=128  超出范围报错
+
+//        2 short短整型 占两个字节
+        short money =3000;
+
+//        3 int 整型默认的类型 占4个字节默认使用的类型
+        int it =231090909;
+
+//        4 long整型 占8个字节
+        long lg=546665671;
+//        随便写的整数默认位int类型 没有超过long的长度,超过了int类型的长度了
+//        需要将一个整数字面量变为long类型,在其后加上L/l
+        long lg2=19999999922200000l;
+
+//        5 浮点型(小数)
+//        float单精度 占4个字节
+//        写的小数点字面量默认为double 需要将一个小数变为float类型在其后加F/f
+            float score=98.5f;
+
+
+//        6 double 双精度占8个字节
+        double score2=999.99;
+
+//        7字符型 char 占2个字节
+        char ch ='a';
+        char ch2 ='中';
+//        char ch3 ='中国';//报错是字符串
+
+//        8 布尔类型 占1位 boolean
+        boolean re=true;
+        boolean re2=false;
+
+        System.out.println("---------------------引用数据类型---------------------");
+
+        String name="西门吹雪";
+        System.out.println(name);
+
+    }
 }
